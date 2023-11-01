ac59defdf81afbc71ef52e814c61454d41d6ce89
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-10-31 23:57:01 -0300

    UP

--- src/app/cuscuz-gourmet/cuscuz-gourmet.component.ts
+++ src/app/cuscuz-gourmet/cuscuz-gourmet.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './cuscuz-gourmet.component.html',
   styleUrls: ['./cuscuz-gourmet.component.css']
 })
-export class CuscuzGourmetComponent {
+export class CuscuzGourmetComponent{
+  pegarValor(valor: string) {
+    let x:number = 0
 
+    if(x === 0){
+      let recheio1:string = valor
+      x = 1
+      console.log("este é o valor 1", recheio1)
+    }else if(x === 1){
+      let recheio2:string = valor
+      x = 0
+      console.log("este é o valor 2", recheio2)
+    }
+  }
 }
