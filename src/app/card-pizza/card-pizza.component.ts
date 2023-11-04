5560fad0db4aa1dd5ea18ef908af8f20ea30b543
Jasintu <128087894+Jasintu@users.noreply.github.com>
2023-11-03 23:38:25 -0300

    UP

--- src/app/card-pizza/card-pizza.component.ts
+++ src/app/card-pizza/card-pizza.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-pizza.component.css']
 })
 export class CardPizzaComponent {
+  pizzasComputadas:Array <string> = [
 
+  ]
+  quantidadeMargherita:number = 1
+  quantidadeDoisQueijos:number = 1
+  quantidadeMussache:number = 1
+  //margherita
+  pegarInfoPizza(NomePedido: string, Quantidade:number, custo:number){
+    console.log(NomePedido, Quantidade, custo)
+  }
+  adicionarMargherita(){
+    this.quantidadeMargherita = ++this.quantidadeMargherita
+  }
+  removerMargherita(){
+    this.quantidadeMargherita = --this.quantidadeMargherita
+    if(this.quantidadeMargherita == 0){
+      this.quantidadeMargherita = 1
+    }
+  }
+  /************************/
+  //Dois Queijos
+  adicionarDoisQueijos(){
+    this.quantidadeDoisQueijos = ++this.quantidadeDoisQueijos
+  }
+  removerDoisQueijos(){
+    this.quantidadeDoisQueijos = --this.quantidadeDoisQueijos
+    if(this.quantidadeDoisQueijos == 0){
+      this.quantidadeDoisQueijos = 1
+    }
+  }
+  /************************/
+  //Mussache
+  adicionarMussache(){
+    this.quantidadeMussache = ++this.quantidadeMussache
+  }
+  removerMussache(){
+    this.quantidadeMussache = --this.quantidadeMussache
+    if(this.quantidadeMussache == 0){
+      this.quantidadeMussache = 1
+    }
+  }
+  /************************/
 }
